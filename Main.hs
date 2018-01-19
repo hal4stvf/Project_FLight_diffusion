966018c055c10322596514d69494c6b58bb2369b
schmeckpeper <hal4stuvf@gmail.com>
2018-01-19 15:12:48 +0100

    kleine Veränderung. Anfang mit der Cursosfarbe zu spielen

--- Main.hs
+++ Main.hs
@@ -11,12 +11,23 @@ move (xdim, ydim) "\"h\"" (x, y) = ((x - 1) `mod` xdim, y)
 move (xdim, ydim) "\"l\"" (x, y) = ((x + 1) `mod` xdim, y)
 move _ _ x = x
 
+-- changecolor :: (Int, Int)  
+
 toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
-toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
+toFrame (xdim, ydim) (x', y') 
+  = ListFrame $ 
+  map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) 
+  [0 .. ydim - 1]
 
 eventTest :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
-eventTest events pixel = (toFrame dim pixel', pixel')
-  where pixel' = foldl (\acc (Event mod ev) -> if mod == "KEYBOARD" then move dim ev acc else acc) pixel events
+eventTest events pixel = (toFrame dim pixel', helper pixel')
+  where 
+    pixel' = foldl (\acc (Event mod ev) -> if mod == "KEYBOARD" then move dim ev acc else acc) pixel events
+    helper = id
+
+colors :: [(Int,Int,Int)]
+--  colors = [(255,0,0),(0,255,0),(0,0,255),(255,255,255)]
+colors = [(255,0,0)]
 
 dim :: (Int, Int)
 dim = (30, 12)
