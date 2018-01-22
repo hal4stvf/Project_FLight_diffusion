3289c079b056a3a600e9b9cef6f4c7608fd1c8a9
schmeckpeper <hal4stuvf@gmail.com>
2018-01-22 19:03:53 +0100

    Nun funktionierdes Programm
    es funktioniert die Veränderung des Cursors, der Farbenwechsel des Cursors und das soweit generisch wie möglich. Als nächstes wird einfache Diffusion implementiert

--- Main.hs
+++ Main.hs
@@ -40,20 +40,12 @@ toFrame (xdim, ydim) myState
     topicture (((x,y), pixelcol): xs)
       | curpos myState == (x,y)    = ((x,y),colors !! curcolcos myState) : (topicture xs) 
       | otherwise                   = ((x,y), pixelcol) : (topicture xs)  
---    con2frame xs = [[c | ((x,y), c) <- xs, x < xdim, y == x2] | ((x1,x2), d) <- xs, x2 < ydim ]
     con2frame [] = []
     con2frame xs = [[ c | ((x,y), c) <- (take (xdim -1) xs) ]] ++ con2frame (drop (xdim -1) xs) 
 
--- = ListFrame $
---  map (\y -> map (\x -> if x == x' && y == y'
---    then Pixel ((colors !! (col `mod` 4)) !! 0) ((colors !! (col `mod` 4)) !! 1) ((colors !! (col `mod` 4)) !! 2)
---    else (if any (==(x,y)) (level !! 0) then Pixel 0xff 0 0
---      else (if any (==(x,y)) (level !! 1) then Pixel 0 0xff 0
---        else Pixel 0 0 0xff))) [0 .. xdim - 1]) [0 .. ydim - 1]
-
 --------------------------------------------------------------------------
--- bekommt eine Liste von Events (siehe Simple.hs) und einen Status (Pixel, Farbe)
--- pixel' wendet eine Fkt. nach und nach auf das Pixel und alle bekommenen Events an
+-- bekommt eine Liste von Events (siehe Simple.hs) und einen Status myState  
+-- helper wendet eine Fkt. nach und nach auf das Pixel und alle bekommenen Events an
 --   wenn etwas eingelesen wird, setzt runMateM (siehe MateLight.hs) das Event "KEYBOARD" [c] (Z. 86/87);
 --   dementsprechend checkt die Funktion, ob das aktuell ausgelesene Event ein KEYBOARD-Event ist
 --   falls ja: checke, ob ein c eingelesen wurde
@@ -71,14 +63,8 @@ eventTest events myState = (toFrame dim (helper events myState), (helper events 
     | mod == "KEYBOARD" && ev == "\"p\""      = helper events (id myState)
     | mod == "KEYBOARD"                       = helper events (move dim ev myState )
     | otherwise                               = helper events (id myState)
-        
---    pixel' = foldl 
---     (\(acc,c, xs) (Event mod ev) -> if mod == "KEYBOARD" then 
---       (if ev == "\"c\"" then changeColor (acc,c,xs) else move dim ev (acc,c,xs)) 
---      else (acc,c,xs)) (pixel, color, xs) events
-    
 
---level = [[(x,y)| y <- [0..11], x <- [0..y]++[29-y..29]],[(x,y)| x <- [8..21], y <- [8..12]],[(x,y)| x <- [22..29], y <- [0..11]]]
+level = [[(x,y)| y <- [0..11], x <- [0..y]++[29-y..29]],[(x,y)| x <- [8..21], y <- [8..12]],[(x,y)| x <- [22..29], y <- [0..11]]]
 --------------------------------------------------------------------------
 
 --------------------------------------------------------------------------
@@ -98,11 +84,10 @@ colors = [Pixel 0xff 0 0, Pixel 0 0xff 0, Pixel 0 0 0xff, Pixel 0xff 0xff 0xff]
 
 -- Feldgröße
 dim :: (Int, Int)
-dim = (12, 30)
---dim = (30, 12) [TODO]
+dim = (30, 12)
 
 --levels
-level1 = [((x,y), Pixel 0 0 255) | x <- [0 .. (fst dim) - 1], y <- [0 .. (snd dim) - 1]]
+level1 = [((x,y), Pixel 0 0 255) | y <- [0 .. (snd dim) - 1], x <- [0 .. (fst dim) - 1]]
 
 -- Anfangsstatus
 anStatus = MyState (0, 0) 3 [((0,0),colors !! 3)] [level1] 0
