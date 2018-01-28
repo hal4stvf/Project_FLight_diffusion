0d301325f4c0556782ee25dbe7b035ee0982b48b
schmeckpeper <hal4stuvf@gmail.com>
2018-01-28 18:30:23 +0100

    Anton möchte sein Code getestet haben. Keine wesentlichen Erneuerung.

--- Diffusion.hs
+++ Diffusion.hs
@@ -12,16 +12,33 @@ import PixelHandling
 -- 
 ---------------------------------------------------
 
+-- Nutz zur Zeit den Algorithmus, das eigentlich nur das Farbfeld detekten soll.
+
 diffusion :: MyState -> MyState
 diffusion myState = myState {
  gameplay        = (curpos myState, colors !! curcolcos myState) : gameplay myState 
-,diffusionstatus = diffOperator (curpos myState) 
-                   (colors !! curcolcos myState)   
+,diffusionstatus = teile $ thesePixel (curpos myState) 
+                   ((levels myState !! curlevel myState) $. (curpos myState))
                    (levels myState !! curlevel myState)
          }
 
+------------------------------
+-- Diffusion Version.				 |
+-- Use    in working version |
+------------------------------
+
+--diffusion :: MyState -> MyState
+--diffusion myState = myState {
+-- gameplay        = (curpos myState, colors !! curcolcos myState) : gameplay myState 
+--,diffusionstatus = diffOperator (curpos myState) 
+--                   (colors !! curcolcos myState)   
+--                   (levels myState !! curlevel myState)
+--         }
+--
+--------------------------------------------------
+
 ---------------------------------------------------
---------------------------------------------------------------------------------
+{------------------------------------------------------------------------------}
 
 {------------------------------------------------------------------------------}
 {----------------------------- Funktionsname ----------------------------------}
@@ -32,66 +49,80 @@ diffusion myState = myState {
 -- (hoffentlich gleich) dynamischen "Diffusionoperator"
 ---------------------------------------------------
 
---   1
--- 1   1
---   1 
-diffQ :: [(Int,Int)]
-diffQ = [(-1,0),(0,-1),(1,0),(0,1)]    
-
--- 2 1 2
--- 1   1
--- 2 1 2 
-diffR :: [(Int,Int)]
-diffR = diffQ $* (sigma0 diffQ) 
-
-($*) xs ys = [x $+ y | (x,y) <- zip xs ys]
-
--- permutate
-sigma0 xs = (tail xs ++ head xs:[])
-sigmaN xs n = iterate sigma0 xs !! n
-sigma xs = sigmaN xs 3
-
--- Nutzlos?
-faltM :: [[(Int,Int)]]
-faltM = [diffQ,diffR] 
-
--- Nutzlos?
-faltIt1 :: [[(Int,Int)]]
-faltIt1 = [diffQ $* diffQ, diffQ $* diffR, diffQ $* (sigmaN diffR 3), diffR $* diffR]
-faltIt2 = [diffQ $* x | x <- faltIt1 ] 
-faltIt3 = [(sigmaN diffR 3) $* x | x <- faltIt1, x /= last faltIt1 ] ++ ((diffR $* (last faltIt1) : []))
-
--- [Purpose] 
--- definiert die Bewegungen
-ita f g x y p = iterate g (iterate f p !! x) !! y
-
-ne = ita north east 
-nw = ita north west 
-sw = ita south west
-se = ita south east 
-
--- hier über muss gecyclt werden
-movingList = [ne,nw,sw,se]
-movingList2 = [ne,ne,nw,nw,sw,sw,se,se]
-
 -- generierte eine Liste an Duppeln, die es gerade braucht um mit der movingliste
 -- eine "Diffusion" zu simmulieren
-pointGenerate n = [ f d (i,n) | i <- [0..n], d <- [0..7], not (i == 0 || i ==n) || (d < 4) ]
+pointGenerate n = [ f d (i,n) | i <- [0..n], d <- [0..7],  if (i == 0 || i == n ) then (d < 4) else True ]
   where 
-    f d (i,n) |not $ odd d = (i,n)
+    f d (i,n) | not $ odd d = (i,n)
               | otherwise = (n,i)
 
-difmat = [ f n (pointGenerate n) | n <- [0..]]
-  where
-    f n xs = [take 4 xs] ++ [[x | x <- take 8 (drop (8*i) (drop 4 xs))] | i <- [0..n-1]]
+-- generierte eine Liste von Liste
+
+-- diffOP für ein ganzes Bild ausgehend von einem Punkt
+-- für eine ganzes Bild
+pointBild = foldr (++) [] [pointGenerate n | n <- [0..29]]
+diffOP point  = [g ys point | ys <- (teileThis pointBild)]
+  where 
+  g ys point 
+     | length ys == 4     
+       = [mv n m point | (mv, (n,m)) <- zip movingList ys, f mv n m point]
+     | otherwise 
+       = [mv n m point | (mv, (n,m)) <- zip movingList2 ys, f mv n m point]
+  f mv n m p | (n == 0 && m == 0)= True
+             | mv n m p == p     = False
+             | otherwise         = True
+--
+--diffOP point =  [ g ys point | ys <- diffmat]) : (rekur (k + 1))
+--g ys point 
+--   | length ys == 4     
+--     = [mv n m point | (mv, (n,m)) <- zip movingList ys, (mv n m point) `inPic` picture]
+--   | otherwise 
+--     = [mv n m point | (mv, (n,m)) <- zip movingList2 ys, (mv n m point) `inPic` picture]
+--inPic p xs = any (p ==) xs 
+--
+--
+
+-- [TODO] für den Effekt noch klein teiliger
+
+teilek _ []  = []
+teilek k xs = [take k xs] ++ teilek k (drop k xs)
+teile = teilek 2
+
+checkForthisnumber n _ j | j >= n = [] 
+checkForthisnumber n i j  = [j,j+i] ++ checkForthisnumber n (i*2) (j+i+1) 
+isThisTrueList = [0,1,2] ++ checkForthisnumber 900  2 3
+
+
+-- Hier muss noch eine ifabfrage rein
+--helperTeileThis n xs = [take 4 xs] ++[(take 4 . drop 4)xs] ++ [[x | x <- take 8 (drop (8*i) (drop 4 xs))] | i <- [0..n-1]]
+helperTeileThis n xs = [[x | x <- selectRight i xs] | i <- [0..n-1]]
+  where 
+     selectRight i | any (i==) isThisTrueList  = (take 4 . drop (i*4))
+                   | otherwise = (take 8 . drop (4*i))
+teileThis = helperTeileThis 900 
+------------------------------
+-- Another Version.					 |
+-- Remove in working version |
+------------------------------
+
+-- für eine ganzes Bild
+--pointBild = foldr (++) [] [pointGenerate n | n <- [0..29]] diffOP point  = [ mv n m point | (mv, (n,m)) <- zip (cycle movingList) pointBild, f mv n m point ]
+--  where 
+--  f mv n m p | mv n m p == p    = False
+--             | otherwise         = True
+--
+--------------------------------------------------
+
+
+--diffopPic point = [mv n m point | (mv, (n,m)) <- zip movingList2 ys ]
 
 -- eigentlich lösung?
 -- [TODO] unbedingt an den Abbruchbedinugen arbeiten, diese sind nur hart implementiert
 --
 trial k point picture = rekur k
   where  
-   rekur k  | k == 3 = []
-   rekur k = (foldr (++) [] [ g ys point | ys <- (head $ drop k difmat)]) : (rekur (k + 1))
+   rekur k  | k == 80 = []
+   rekur k = (foldr (++) [] [ g ys point | ys <- teile $ pointGenerate k]) : (rekur (k + 1))
    g ys point 
       | length ys == 4     
         = [mv n m point | (mv, (n,m)) <- zip movingList ys, (mv n m point) `inPic` picture]
@@ -100,29 +131,14 @@ trial k point picture = rekur k
    inPic p xs = any (p ==) xs 
 
 --------------------------------------------------
-
-  
--- berechnet die nte "faltung" (lazy ;)) 
--- [TODO] Hier fehlt die berücksichtigung, dass DR == RD ist 
--- und mann eigentlich D3(R) schreiben muss. nur für gerades n?
-faltIteration xs n =  iterate falt_each xs !! n
+------------------------------
+-- Another Version.					 |
+------------------------------
+difmat = [ f n (pointGenerate n) | n <- [0..]]
   where
-    falt_each xs = [f m x | m <- faltM, x <- xs]
-    f = ($*)
---    f m x | m == diffQ    = m $* x
---          | m == diffR && x == diffR   = m $* x
---          | odd n               = m $* (sigmaN x 1)
---          | even n              = m $* (sigmaN x 3)
-    
-
--- speziell für faltM
--- [TODO] hier noch die mittleren Listen in einer sinnvolleren Art zusammen-
--- führen. Das heißt auf 2 mal 4*(n-2) große Liste ... nur in richtig(z.B.)
-nteFaltIt n = f $ faltIteration faltM n
-  where 
-    f = id
---    f x  = (head x) : foldr (++) [] (tailnoLast x) : last x :[] 
---    tailnoLast xs = [x | x<- xs, x /= last xs]
+    f n xs = [take 4 xs] ++ [[x | x <- take 8 (drop (8*i) (drop 4 xs))] | i <- [0..n-1]]
+--
+--------------------------------------------------
 {------------------------------------------------------------------------------}
 
 {------------------------------------------------------------------------------}
@@ -132,10 +148,7 @@ nteFaltIt n = f $ faltIteration faltM n
 -- Gibt am Ende all die Pixel aus, die gefärbt werden sollen
 ---------------------------------------------------
 
--- [TODO] Hier eine Funktion schreiben, die ausgehend von einem Punkt
 -- das Spielfeld abgeht ... 
-
-thesePixel = selectColor 
 
 data SelectColor = SelectColor {
    lastpoint    :: (Int, Int) 
@@ -143,40 +156,124 @@ data SelectColor = SelectColor {
   ,sellevel     :: [((Int,Int),Pixel)]
   ,startpoint   :: (Int,Int)
   ,color        :: Pixel
+  ,level        :: [((Int,Int),Pixel)] 
   } deriving (Eq, Ord, Show, Read)
 
--- [TODO] Hier muss wohl die Abbruchbedingung noch einmal überarbeitet werden
---selectColor :: (Int,Int) -> Pixel -> [((Int, Int),Pixel)] -> SelectColor 
-selectColor point color level = [p | (p,c) <- sellevel $ rekursion curSelCol]
-  where  
-    rekursion x | startpoint x == f (currentpoint x) x       = x
-    rekursion x = rekursion  x { lastpoint = currentpoint x, currentpoint = f (currentpoint x) x, 
-      sellevel = g (sellevel x) x}
-    f p x
-      | (north p /= p) && ((level $. (north p)) == color) && (h x (north p)) = north p
-      | (west  p /= p) && ((level $. (west  p)) == color) && (h x (west  p)) = west  p
-      | (south p /= p) && ((level $. (south p)) == color) && (h x (south p)) = south p
-      | (east  p /= p) && ((level $. (east  p)) == color) && (h x (east  p)) = east  p
-      | otherwise = startpoint curSelCol
-    g xs x= (currentpoint x, color) : xs
-    h x a = not $ any (\ (p,c) -> p == a) (sellevel x)
-    firstedge point | gofurhter point      = firstedge $ north point
-                    | otherwise            = point 
-    gofurhter point | north point /= point && level $. point == color = True 
-                    | otherwise                                       = False
+-- [TODO] Hier muss wohl die Startbedingung noch einmal überarbeitet werden
+-- Übergibt ein SelectColorRecord an getField mit initaliziertem Startvektor
+-- und den Farben etc.
+-- 
+thesePixel point color level = [p | (p,c) <- sellevel $ getField curSelCol] 
+ where
     curSelCol :: SelectColor
-    curSelCol = SelectColor (firstedge point) (firstedge point) [] (firstedge point) color
+    curSelCol = SelectColor (point) (point) [(point, color)] (point) color level
+--    curSelCol = SelectColor (firstedge point) (firstedge point) [(firstedge point, color)] (firstedge point) color level
+--    firstedge point | gofurhter point      = firstedge $ north point
+--                    | otherwise            = point 
+--    gofurhter point | north point /= point && level $. north point == color = True 
+--                    | otherwise                                       = False
 
--- Moves zum abgehen des Feldes
+-- [TODO] Hier muss wohl die Abbruchbedingung noch einmal überarbeitet werden
+-- Versucht alle Felder ausgehend von einem Startvektor zu erreichen 
+getField mySC | endOfField $ currentpoint mySC  = mySC
+              | otherwise 
+    = getField mySC { lastpoint = g , currentpoint = f (currentpoint mySC), 
+      sellevel = savePoint (sellevel mySC)}
+  where
+    f p 
+      | goFurther north p  = north p
+      | goFurther west  p  = west  p
+      | goFurther south p  = south p
+      | goFurther east  p  = east  p
+    savePoint xs = (currentpoint mySC, color mySC) : xs
+--    visitBefore a = not $ any (\ (p,c) -> p == a) $ (take 1 . reverse) $ sellevel mySC
+    visitBefore a = a == currentpoint mySC 
+    goFurther moveto p 
+      = (moveto p /= p) && ((level mySC $. (moveto p)) == color mySC) && (visitBefore (moveto p))
+    endOfField p =  (not $ goFurther north p )
+                 && (not $ goFurther west p ) 
+                 && (not $ goFurther south p) 
+                 && (not $ goFurther east p )
+    g = currentpoint mySC          
+
+
+
+
+
+
+------------------------------
+-- Another Version.					 |
+-- Remove in working version |
+------------------------------
+
+---- [TODO] Hier muss wohl die Abbruchbedingung noch einmal überarbeitet werden
+----selectColor :: (Int,Int) -> Pixel -> [((Int, Int),Pixel)] -> SelectColor 
+--selectColor point color level = [p | (p,c) <- sellevel $ rekursion curSelCol]
+--  where  
+--    rekursion x | startpoint x == f (currentpoint x) x       = x
+--    rekursion x = rekursion  x { lastpoint = currentpoint x, currentpoint = f (currentpoint x) x, 
+--      sellevel = g (sellevel x) x}
+--    f p x
+--      | goFurther north p x = north p
+--      | goFurther west  p x = west  p
+--      | goFurther south p x = south p
+--      | goFurther east  p x = east  p
+--      | otherwise = startpoint curSelCol
+--    g xs x= (currentpoint x, color) : xs
+--    h x a = not $ any (\ (p,c) -> p == a) (sellevel x)
+--    firstedge point | gofurhter point      = firstedge $ north point
+--                    | otherwise            = point 
+--    gofurhter point | north point /= point && level $. north point == color = True 
+--                    | otherwise                                       = False
+--    curSelCol :: SelectColor
+--    curSelCol = SelectColor (firstedge point) (firstedge point) [(firstedge point, color)] (firstedge point) color
+--    goFurther moveto p x
+--      = (moveto p /= p) && ((level $. (moveto p)) == color) && (h x (moveto p))
 --
-north (x,y) | y-1 >= 0          = (x,y-1)
-            | otherwise         = (x,y)
-west  (x,y) | x+1 <= fst dim    = (x+1,y)
-            | otherwise         = (x,y)
-south (x,y) | y+1 <= snd dim    = (x,y+1)
-            | otherwise         = (x,y)
-east  (x,y) | x-1 >= 0          = (x-1,y)
-            | otherwise         = (x,y)
+--------------------------------------------------
+    
+
+-- Moves zum abgehen des Feldes in k Schritten
+
+northk k (x,y)  | y-k >= 0          = (x,y-k)
+                | otherwise         = (x,y)
+westk  k (x,y)  | x+k <= fst dim    = (x+k,y)
+                | otherwise         = (x,y)
+southk k (x,y)  | y+k <= snd dim    = (x,y+k)
+                | otherwise         = (x,y)
+eastk  k (x,y)  | x-k >= 0          = (x-k,y)
+                | otherwise         = (x,y)
+
+-- Moves in einer Schritten
+
+north = northk 1
+west  = westk 1
+south = southk 1
+east  = eastk 1
+
+
+-- Hilfsfunktion zum "iterieren" über die moves in verschiedene Richtung
+ita f g x y p = f x (g y p) 
+
+ne n m p 
+  | checkFormv northk eastk n m p  = ita northk eastk n m p 
+  | otherwise                          = p
+nw n m p 
+  | checkFormv northk westk n m p  = ita northk westk n m p 
+  | otherwise                        = p
+sw n m p 
+  | checkFormv southk westk n m p  = ita southk westk n m p
+  | otherwise                        = p
+se n m p 
+  | checkFormv southk eastk n m p  = ita southk eastk n m p 
+  | otherwise                          = p
+
+checkFormv f g n m p | (f n p /= p || n /= 0) && (g m p == p || m == 0) = True
+                     | otherwise                                        = False
+
+-- hier über muss gecyclt werden
+movingList = [ne,nw,sw,se]
+movingList2 = [ne,ne,nw,nw,sw,sw,se,se]
 
 {------------------------------------------------------------------------------}
 
@@ -192,72 +289,7 @@ east  (x,y) | x-1 >= 0          = (x-1,y)
 diffOperator :: (Int,Int) -> Pixel -> [((Int,Int),Pixel)] -> [[(Int,Int)]]
 diffOperator point color picture = trial 0 point (thesePixel point (picture $. point) picture)
 
-------------------------------
--- Another Version.					 |
--- Remove in working version |
-------------------------------
-
---mat = diffQ ++ diffR 
---diffMat ms = makeuniq (((0,0):mat) ++ foldr (++) [] (mapQ ms ++ mapR ms))
---  where
---    mapQ = map (diffofpoint diffQ) 
---    mapR = map (diffofpoint diffR) 
---    makeuniq [] = []
---    makeuniq (x:xs) 
---      | sum [if x==y then 1 else 0 | y <-xs] /= 0  = x : makeuniq [z | z <-xs, x /= z]
---      | otherwise                                  = x : makeuniq xs 
---    diffofpoint ::  [(Int,Int)] -> (Int, Int) -> [(Int,Int)]
---    diffofpoint = 
---      (\ ys x -> [x $+ y | y <- ys, not $ any ((x $+ y) ==) ((0,0):ys++mat), sct (x $+ y) ]) 
---    sct :: (Int, Int) -> Bool
---    sct (x,y) | abs (x) > fst dim || abs (y) > snd dim    = False
---              | otherwise                                 = True
---
---diffMatRek ms n = iterate diffMat ms !! n 
---
---diffOP  = diffMatRek mat 6
---diffOPvar n = diffMatRek mat n 
---
---diffOperator :: (Int,Int) -> Pixel -> [((Int,Int),Pixel)] -> [[(Int,Int)]]
---diffOperator point color picture = h $ g diffOP
---  where
---    g [] = []
---    g (x:xs) 
---      | not $ null [1 | (p,c) <- picture, p == (x $+ point), c /= color] 
---                  = (x $+ point)  : g xs  
---      | otherwise = g xs 
---    h [] = []
---    h ws = (take 4 ws) : (h $ drop 4 ws)
-----
---------------------------------------------------
-
----------------------------------------------------
---------------------------------------------------------------------------------
-
-
-------------------------------
--- Reduante Version.	  		 |
--- Remove in working version |
-------------------------------
-
---diffMatrix1 xs = xs ++ helper (xs ++ take 1 xs)  
---  where
---  helper xs | length xs < 2 = [] 
---  helper xs = add2ListDup xs : (helper $ tail xs)
---  add2ListDup ys = (\ ((x1,y1):(x2,y2):xs) -> (x1+x2,y1+y2)) ys
-
----- Fehlerhafte zu manueller Versuch
---diffMatrix2 xs = [(x+x,y+y) | (x,y) <- xs]
---diffMatrix3 xs = (take 4 xs) +++  (drop 4 xs)  
---
---diffMatrix xs = diffMatrix1 xs ++ diffMatrix2 xs
---
---(+++) _ [] = []
---(+++) [] _ = []
---(+++) xs ys = (\ ((x1,y1):qs) ((x2,y2):ws) -> (x1+x2,y1+y2): (qs +++ ws)) xs ys   
---
---------------------------------------------------
---------------------------------------------------------------------------------
+{------------------------------------------------------------------------------}
 
 
 {------------------------------------------------------------------------------
@@ -274,10 +306,10 @@ oneStepDiffusion myState = myState {diffusionstatus = tail $ diffusionstatus myS
   where 
       f = h diffstat   
       h [] x = x
-      h (d:ds) s = h ds (l d s)
-      l d = map (\(p,c) -> if (p /= d) then (p,c) else (p,newc)) 
+      h (d:ds) s = h ds (selectpixelmap d s)
+      selectpixelmap d = map (\(p,c) -> if (p /= d) then (p,c) else (p,newcolor)) 
       diffstat = head $ diffusionstatus myState
-      newc = snd $ head $ gameplay myState
+      newcolor = snd $ head $ gameplay myState
 
 ---------------------------------------------------
 --------------------------------------------------------------------------------
